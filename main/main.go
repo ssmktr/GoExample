3a2bb1901478351b4f9f71637d3b994735703d0a
ssmkt <ssmktr@naver.com>
2018-08-18 15:56:36 +0900

    no message

--- main/main.go
+++ main/main.go
@@ -1,69 +1,19 @@
 package main
 
 import (
-	"encoding/gob"
-	"fmt"
-	"net"
-
+	"net/http"
 	"github.com/unrolled/render"
 )
-
-func server() {
-	ln, err := net.Listen("tcp", ":2305")
-	if err != nil {
-		fmt.Printf("Error Listen : %v\n", err)
-		return
-	}
-	defer ln.Close()
-
-	for {
-		c, err := ln.Accept()
-		if err != nil {
-			fmt.Printf("Error Accept : %v\n", err)
-			continue
-		}
-
-		handleServerConnection(c)
-	}
-}
-
-func handleServerConnection(c net.Conn) {
-	defer c.Close()
-
-	for {
-		var msg string
-		err := gob.NewDecoder(c).Decode(&msg)
-		if err != nil {
-			fmt.Printf("Error Decode : %v\n", err)
-		} else {
-			fmt.Printf("Received : %v\n", msg)
-		}
-	}
-}
-
-func client() {
-	c, err := net.Dial("tcp", "127.0.0.1:2305")
-	if err != nil {
-		fmt.Printf("Error Dial : %v\n", err)
-		return
-	}
-	defer c.Close()
-
-	for {
-		var msg string
-		fmt.Scanln(&msg)
-		fmt.Println("Sending", msg)
-		err = gob.NewEncoder(c).Encode(msg)
-		if err != nil {
-			fmt.Printf("Error Sending : %v\n", err)
-			return
-		}
-	}
-}
 
 var renderer render.Render
 
 func main() {
-	// server()
-	client()
+
+	http.HandleFunc("/", func (res http.ResponseWriter, req *http.Request) {
+		renderer.Text(res, http.StatusOK, "SSMKTR")
+	})
+
+	http.ListenAndServe(":2305", nil)
 }
+
+
