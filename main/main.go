c34d5be0e4947fd4ef1520f14b3d806270b003f8
ssmktr <ssmktr@naver.com>
2018-08-20 15:53:19 +0900

    test

--- main/main.go
+++ main/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-		"net/http"
-	"github.com/unrolled/render"
+			"github.com/unrolled/render"
+	"fmt"
 )
 
 var renderer render.Render
 
 func main() {
 
-	//fmt.Println("SSMKTR")
+	fmt.Println("SSMKTR")
 
-	http.HandleFunc("/", func (res http.ResponseWriter, req *http.Request) {
-		renderer.Text(res, http.StatusOK, "SSMKTR")
-	})
-
-	http.ListenAndServe(":2305", nil)
+	//http.HandleFunc("/", func (res http.ResponseWriter, req *http.Request) {
+	//	renderer.Text(res, http.StatusOK, "SSMKTR")
+	//})
+	//
+	//http.ListenAndServe(":2305", nil)
 }
 
 
