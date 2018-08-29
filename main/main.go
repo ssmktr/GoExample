b494909c630a9fd185007a2a4a7883443c63117a
ssmktr <ssmktr@naver.com>
2018-08-29 11:15:34 +0900

    test

--- main/main.go
+++ main/main.go
@@ -1,54 +1,52 @@
 package main
 
 import (
-	"net"
-	"fmt"
-	"github.com/unrolled/render"
-	"database/sql"
+		"fmt"
+		"database/sql"
 )
 
-var renderer render.Render
-
-func onServer() {
-	listener, err := net.Listen("tcp", ":2305")
-	if err != nil {
-		fmt.Errorf("Error listen : %v\n", err)
-		return
-	}
-	defer listener.Close()
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Errorf("Error Accept : %v\n")
-			return
-		}
-		defer conn.Close()
-
-		fmt.Printf("Connect remoteAddr : %v, localAddr : %v\n", conn.RemoteAddr().String(), conn.LocalAddr().String())
-
-		go func (conn net.Conn) {
-			data := make([]byte, 1024)
-
-			for {
-				n, err := conn.Read(data)
-				if err != nil {
-					fmt.Errorf("Error Read : %v\n", err)
-					return
-				}
-
-				fmt.Println(string(data[:n]))
-
-				_, err = conn.Write(data[:n])
-				if err != nil {
-					fmt.Errorf("Error Write : %v\n", err)
-					return
-				}
-
-			}
-		}(conn)
-	}
-}
+//var renderer render.Render
+//
+//func onServer() {
+//	listener, err := net.Listen("tcp", ":2305")
+//	if err != nil {
+//		fmt.Errorf("Error listen : %v\n", err)
+//		return
+//	}
+//	defer listener.Close()
+//
+//	for {
+//		conn, err := listener.Accept()
+//		if err != nil {
+//			fmt.Errorf("Error Accept : %v\n")
+//			return
+//		}
+//		defer conn.Close()
+//
+//		fmt.Printf("Connect remoteAddr : %v, localAddr : %v\n", conn.RemoteAddr().String(), conn.LocalAddr().String())
+//
+//		go func (conn net.Conn) {
+//			data := make([]byte, 1024)
+//
+//			for {
+//				n, err := conn.Read(data)
+//				if err != nil {
+//					fmt.Errorf("Error Read : %v\n", err)
+//					return
+//				}
+//
+//				fmt.Println(string(data[:n]))
+//
+//				_, err = conn.Write(data[:n])
+//				if err != nil {
+//					fmt.Errorf("Error Write : %v\n", err)
+//					return
+//				}
+//
+//			}
+//		}(conn)
+//	}
+//}
 
 func main() {
 	//onServer()
